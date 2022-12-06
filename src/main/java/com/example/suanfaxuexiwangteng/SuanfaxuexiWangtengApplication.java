bd2ba2a9198a750fd7c7e80e63f868804d51152e
wangteng1993 <2264645267@qq.com>
2022-12-06 15:02:31 +0800

    init

---
+++ src/main/java/com/example/suanfaxuexiwangteng/SuanfaxuexiWangtengApplication.java
@@ -0,0 +1,13 @@
+package com.example.suanfaxuexiwangteng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuanfaxuexiWangtengApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SuanfaxuexiWangtengApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/suanfaxuexiwangteng/SuanfaxuexiWangtengApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.suanfaxuexiwangteng;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuanfaxuexiWangtengApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
