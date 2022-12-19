95a3b6ebad202554fd50b405303c3747e133f42e
wangteng <2264645267@qq.com>
2022-12-19 16:06:25 +0800

    堆排序

---
+++ src/main/java/com/example/suanfaxuexiwangteng/排序算法/leetCode/_001按身高排序.java
@@ -0,0 +1,16 @@
+package com.example.suanfaxuexiwangteng.数组.leetcode;
+
+/**
+ * 给你一个字符串数组 names ，和一个由 互不相同 的正整数组成的数组 heights 。两个数组的长度均为 n 。
+ *
+ * 对于每个下标 i，names[i] 和 heights[i] 表示第 i 个人的名字和身高。
+ *
+ * 请按身高 降序 顺序返回对应的名字数组 names 。
+ *
+ *
+ * 输入：names = ["Mary","John","Emma"], heights = [180,165,170]
+ * 输出：["Mary","Emma","John"]
+ * 解释：Mary 最高，接着是 Emma 和 John 。
+ */
+public class _001按身高排序 {
+}
