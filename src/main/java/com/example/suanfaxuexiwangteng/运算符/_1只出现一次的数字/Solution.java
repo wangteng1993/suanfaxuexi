3b55c75a23f13cf1e980415bdec94aeb9882ea20
wangteng <2264645267@qq.com>
2022-12-19 15:27:52 +0800

    堆排序

---
+++ src/main/java/com/example/suanfaxuexiwangteng/运算符/_1只出现一次的数字/Solution.java
@@ -0,0 +1,19 @@
+package com.example.suanfaxuexiwangteng.数组._1只出现一次的数字;
+
+/**
+ * 一个出现一次 其余都出现两次 找出出现一次的数据
+ */
+public class Solution {
+    public static void main(String[] args) {
+        int[] ints = {1, 1,4,4,6,7,6,7,8,10,10};
+        System.out.println(one(ints));
+    }
+    static int one(int[] nums){
+
+        int result=0;
+        for (int num : nums) {
+            result=result^num;
+        }
+        return result;
+    }
+}
